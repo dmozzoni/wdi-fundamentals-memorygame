47252a8c96e1dc70413a45a7a238a9311bba1947
dmozzoni <cyric@hotmail.com>
2016-11-12 01:43:27 -0500

    init JS card function

--- starter-code/script/main.js
+++ starter-code/script/main.js
@@ -1,5 +1,28 @@
 // Card declarations and values
+var cardNum = 4;
+var name = 'game-board'
 var cardOne = "queen";
 var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
+
+//if (cardOne === cardTwo) {
+//	alert("You Found a Match!");
+//} else {
+//	alert("Sorry, Try again.");
+//}
+//(cardOne === cardThree) ? alert("Yay") : alert("Nay");
+
+//    <div class="card"></div>
+
+var createCards = function(elementID,num) {
+  for (var i= 1; i<=num; i++) {
+    var aCard = document.createElement('div');
+    aCard.className = 'card';
+    elementID.appendChild(aCard);
+  }
+}
+
+createCards(document.getElementById('game-board'),cardNum);
+
+
